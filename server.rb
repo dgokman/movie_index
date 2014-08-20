c24ae0b6ccede603172eff9977ac3f89b1eb81f0
Dennis Gokman <dennis.gokman@gmail.com>
2014-08-20 12:08:08 -0400

    sorted titles and added css

--- server.rb
+++ server.rb
@@ -1,26 +1,3 @@
-# require 'sinatra'
-# require 'csv'
-# require 'pry'
-#   movies = []
-#    CSV.foreach('movies.csv', headers: true, header_converters: :symbol) do |row|
-#     movies << row.to_hash
-#    end
-#    #binding.pry
-#   def get_title(movies)
-#     titles = []
-#     movies.each do |title|
-#      titles << title[:title]
-#     end
-#     titles
-#   end
-
-#   get '/movies' do
-#    #@movies = movies
-#    @movie = get_title(movies)
-#    erb :index
-#    #binding.pry
-#   end
-
 require 'sinatra'
     require 'csv'
     require 'pry'
@@ -29,30 +6,35 @@ require 'sinatra'
         movies << row.to_hash
       end
 
-    def get_title(movies)
-      titles = []
-      movies.each do |title|
-        titles << title[:title]
-      end
-      titles
-    end
+    # def get_title(movies)
+    #   titles = []
+    #   movies.each do |title|
+    #     titles << title[:title]
+    #   end
+    #   titles
+    # end
 
-    def get_ids(movies)
-      ids = []
-      movies.each do |id|
-        ids << id[:id]
-      end
-      ids
-    end
+    # def get_ids(movies)
+    #   ids = []
+    #   movies.each do |id|
+    #     ids << id[:id]
+    #   end
+    #   idsget
+    # end
 
 
     get '/movies' do
       @movies = movies
-      @titles = get_title(movies)
-      @ids = get_ids(movies)
-        @ids.each do |id|
-          @idurl = id
-        end
+
+      #binding.pry
+      sorted_movies = @movies.sort_by {|hash, value| hash[:title] }
+      @sorted_movies = sorted_movies
+      # binding.pry
+      # @titles = get_title(movies)
+      # @ids = get_ids(movies)
+      #   @ids.each do |id|
+      #     @idurl = id
+        # end
       erb :index
     end
 
